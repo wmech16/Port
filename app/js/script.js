6a0bb52360da774bfd34f3bd869844987329bf59
Billy Mech <wmech16@gmail.com>
2017-07-29 15:39:06 -0400

    finished portfolio, started contact

--- app/js/script.js
+++ app/js/script.js
@@ -1,12 +1,24 @@
-$(function () {
-    $(".scroll").effect("bounce", { times:2, distance: 10 }, 500);
-});
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
 
-$(window).bind('scroll', function() {
-     if ($(window).scrollTop() > 15) {
-         $('.scroll').fadeOut();
-     }
-     else {
-         $('.scroll').fadeIn();
-     }
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 500, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    }
+  });
 });
